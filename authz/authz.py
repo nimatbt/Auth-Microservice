8eb66a3813d315f547a279e019180c200d91791f
nima2 <nima2@localhost>
2022-01-12 18:04:44 +0330

    adding restful api v1 structure

--- authz/authz.py
+++ authz/authz.py
@@ -1,8 +1,15 @@
-from flask import Flask
-
+from flask import Flask, Blueprint
+from flask_restful import Api
+ 
 from authz.config import Config
+
+apiv1_bp = Blueprint("apiv1_bp", __name__, url_prefix="/api/v1")
+apiv1 = Api(apiv1_bp)
+
+from authz import resource
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    app.register_blueprint(apiv1_bp)
     return app
---
+++ authz/resource/apiv1/__init__.py
@@ -0,0 +1,1 @@
+from authz.authz import apiv1 as api
