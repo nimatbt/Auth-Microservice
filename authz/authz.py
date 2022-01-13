5e351d2936b018223e971b41a5ade33187897be8
nima2 <nima2@localhost>
2022-01-13 20:59:51 +0330

    adding sqlalchemy structure

--- authz/authz.py
+++ authz/authz.py
@@ -1,7 +1,13 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
+
+from flask_sqlalchemy import SQLAlchemy # 17-1 : 32'
+from flask_migrate import Migrate       # 17-1 : 32'
  
 from authz.config import Config
+
+db = SQLAlchemy() # 17-1 : 33'
+mg = Migrate()    # 17-1 : 33'
 
 apiv1_bp = Blueprint("apiv1_bp", __name__, url_prefix="/api/v1")
 apiv1 = Api(apiv1_bp)
@@ -11,5 +17,7 @@ from authz import resource
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    db.init_app(app)      # 17-1 : 34'
+    mg.init_app(app, db)  # 17-1 : 34'
     app.register_blueprint(apiv1_bp)
     return app
--- authz/config/config.py
+++ authz/config/config.py
@@ -2,11 +2,22 @@ from os import environ
 
 class Config:
 	
+	#################### Aplication Config ###########################
+	
 	ENV = environ.get("AMIMA_AUTHZ_ENV", "production")
 	
 	DEBUG = bool(int(environ.get("AMIMA_AUTHZ_DEBUG", "0")))
 	
 	TESTING = bool(int(environ.get("AMIMA_AUTHZ_TESTING", "0")))
 	
-	SECRET_KEY = environ.get("AMIMA_AUTHZ_SECRET_KEY", "secret-key")
+	SECRET_KEY = environ.get("AMIMA_AUTHZ_SECRET_KEY", "SECRET-KEY")
 	
+	################### Database Config ####### 17-1 : 37' ############
+	
+	SQLALCHEMY_DATABASE_URI = environ.get("AMIMA_AUTHZ_DATABASE_URI", None)
+	
+	SQLALCHEMY_ECHO = DEBUG
+	
+	SQLALCHEMY_RECORD_QUERIES = DEBUG
+	
+	SQLALCHEMY_TRACK_MODIFICATIONS = DEBUG
