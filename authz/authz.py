e6d3b219c7c1d4bad2b428c7f3d235fad2fea2a5
nima2 <nima2@localhost>
2022-02-10 21:18:10 +0330

    adding marshmallow package

--- authz/authz.py
+++ authz/authz.py
@@ -1,13 +1,16 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
 
+from flask_marshmallow import Marshmallow # 19-1 : 7'
+
 from flask_sqlalchemy import SQLAlchemy # 17-1 : 32'
 from flask_migrate import Migrate       # 17-1 : 32'
- 
+
 from authz.config import Config
 
 db = SQLAlchemy() # 17-1 : 33'
 mg = Migrate()    # 17-1 : 33'
+ma = Marshmallow() # 19-1 : 7'
 
 apiv1_bp = Blueprint("apiv1_bp", __name__, url_prefix="/api/v1")
 apiv1 = Api(apiv1_bp)
@@ -19,5 +22,6 @@ def create_app():
     app.config.from_object(Config)
     db.init_app(app)      # 17-1 : 34'
     mg.init_app(app, db)  # 17-1 : 34'
+    ma.init_app(app)      # 19-1 : 7' Add marshmallow to app
     app.register_blueprint(apiv1_bp)
     return app
